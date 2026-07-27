c93f3d4b1054def3a52d49f41a7fa8d98133c34a
dev-info-satyamkr <sy250957@gmail.com>
2026-07-27 18:59:45 +0530

    types of Conversion

---
+++ conversion.java
@@ -0,0 +1,14 @@
+class conversion
+{
+public static void main(String args[])
+{
+   //byte b = 127;
+  // int a = 257;
+  // byte k = (byte) a;
+
+   float f = 5.6f;
+   int t = (int)f;
+   System.out.println(t);
+
+}     
+}
