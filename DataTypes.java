78a21010101d8ffeea30a180faf34de42f04fe12
dev-info-satyamkr <sy250957@gmail.com>
2026-07-27 15:52:58 +0530

    Data Types java

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+class hello
+{
+    public static void main(String a[])
+{
+    int num1 = 9;
+    byte by = 127;
+    short sh = 559;
+    long l = 12478l;
+
+    float f =3.17f;
+    double d = 3.17;
+
+    char c = 'A';
+
+    boolean b = true;
+    
+
+}
+}
