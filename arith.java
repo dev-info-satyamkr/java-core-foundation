5e94b62568c31fd629dae19b49f6f9a9a5e5cd2c
dev-info-satyamkr <sy250957@gmail.com>
2026-07-27 19:48:14 +0530

    Arithmetic operator

---
+++ arith.java
@@ -0,0 +1,17 @@
+public class arith {
+    public static void main(String args[])
+    {
+        int num1 = 4;
+       // int num2 = 7;
+
+       // int result = num1 + num2;
+       // num1 %= 1;
+       // num1++;                             //post Increment 
+        //num1--;                             //Decrement
+        // ++num;                              //pre increment          
+          int result = num1++;          // fetch the value and then increment 
+          //int result = ++num1;           // first increment and then fetch
+        System.out.println(result);
+
+    }
+}
