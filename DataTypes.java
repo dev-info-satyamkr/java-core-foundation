c84fc5967f387265d20282e3dfb0f46fa36c3599
dev-info-satyamkr <sy250957@gmail.com>
2026-07-27 18:35:37 +0530

    Literals in Java

--- DataTypes.java
+++ DataTypes.java
@@ -1,4 +1,4 @@
-class hello
+class DataTypes
 {
     public static void main(String a[])
 {
---
+++ Literal.java
@@ -0,0 +1,34 @@
+class Literal
+{
+    public static void main(String a[])
+    {
+         //Literals
+
+         //int num1 = 0b101;
+         //System.out.println(num1);
+
+         //hexadecimal literal
+         //int num2 = 0x7A;
+         //System.out.println(num2);
+
+         //double
+          // double num3 = 56;
+           //System.out.println(num3);
+
+           //Bolean literal 
+           //double num4 = 12e10;
+           //System.out.println(num4);
+
+           //character literal 
+           char c= 'A';
+           c++;
+           System.out.println(c);
+
+
+
+
+
+
+    }
+  
+}
