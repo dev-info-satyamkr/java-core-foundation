6752bf0f801005043dd34c0958c4bb51424e9d00
dev-info-satyamkr <sy250957@gmail.com>
2026-07-29 10:33:32 +0530

    Logical Operator

---
+++ logical.java
@@ -0,0 +1,20 @@
+public class logical 
+ {
+    public static void main(String [] args)
+     {
+        int a = 6;
+        int b = 7;
+        int x = 11;
+        int y = 14;
+
+       // boolean result = a < b && y > x;
+    //    boolean result = a < b || y > x || a > 1;
+        boolean result = x < y;
+
+          System.out.println( !result);
+
+
+
+
+    }
+}
