010347e159d09bc173508f78521bab1107ab8699
dev-info-satyamkr <sy250957@gmail.com>
2026-07-29 11:32:50 +0530

    today's topic / exercise name

---
+++ ifelseif.java
@@ -0,0 +1,17 @@
+public class ifelseif
+ {
+ public static void main(String[] args)
+ {
+    int x = 9;
+    int y = 11;
+    int z = 14;
+
+     if(x>y && x>z)
+      System.out.println(x);
+     else if(y>z)
+        System.out.println(y);
+      else 
+        System.out.println(z);
+
+ }   
+}
