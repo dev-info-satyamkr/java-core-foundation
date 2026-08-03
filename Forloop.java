5eddf88be8d0e2a638de5c9715f9c2f5e01bcec5
dev-info-satyamkr <sy250957@gmail.com>
2026-08-03 15:25:42 +0530

    ForLoop Java

---
+++ Forloop.java
@@ -0,0 +1,27 @@
+public class Forloop {
+    public static void main(String a[] )
+    {
+        for(int i=1; i<=7; i++)
+        {
+            System.out.println("Hi" + i);
+            {
+                if(i<=5)
+                {
+                    System.out.println("Hello" + i);
+                }
+                else
+                {
+                    System.out.println("Bye" + i);
+
+                }
+                }
+
+            }
+        }
+
+    }
+
+
+
+
+
