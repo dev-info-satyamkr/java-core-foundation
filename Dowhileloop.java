90e8777986f17b5f7bcd5b891faf5768d987b0fd
dev-info-satyamkr <sy250957@gmail.com>
2026-08-03 14:48:09 +0530

    Do While loop Java

---
+++ Dowhileloop.java
@@ -0,0 +1,15 @@
+public class Dowhileloop {
+    public static void main(String[] args)
+    {
+        int i = 5;
+         
+        do
+        {
+            System.out.println("Hi" + 1);
+            i++;
+
+        }while(i<=4);
+
+    }
+
+}
