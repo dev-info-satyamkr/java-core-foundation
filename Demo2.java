6024c7bdf69e2a5d07f7e266f8fb275f8c417dce
dev-info-satyamkr <sy250957@gmail.com>
2026-08-07 23:18:43 +0530

    Static variable in java

--- Demo2.java
+++ Demo2.java
@@ -1,32 +1,31 @@
-class Demo
+// static variable in java 
+
+class Mobile
 {
   String brand;
   int price;
-   String name;
+  static String name;
 
   public void show()
   {
     System.out.println(brand + " : " + price + " : " + name);
   }
 }
-class Mobile extends Demo
+class Demo2
 {
     public static void main (String a[])
     {
       Mobile obj1 = new Mobile();
       obj1.brand = " Samsung ";
       obj1.price = 300000;
-      obj1.name = " Smartphone";
+      Mobile.name = " Smartphone";
 
 
       Mobile obj2 = new Mobile();
       obj2.brand = " Iphone ";
       obj2.price = 200000;
-      obj2.name = " Smartphone";
+      Mobile.name = " Smartphone";
       
-      obj1.name = " Phone ";
-      
-
       obj1.show();
       obj2.show();
     }
