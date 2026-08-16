8d73075d9958b0890696fc5712e753d0796d3804
dev-info-satyamkr <sy250957@gmail.com>
2026-08-16 12:13:46 +0530

    Method Overriding in Java

---
+++ series52.java
@@ -0,0 +1,27 @@
+class Calc
+{
+ public int add(int n1, int n2)
+ {
+    return n1+n2;
+ }    
+
+}
+
+class AdvCalc extends Calc
+{
+    public int add( int n1, int n2)
+    {
+         return n1+n2+1;
+    }
+}
+
+public class series52
+{
+    public static void main(String a[])
+    {
+        AdvCalc obj = new AdvCalc();
+        int r1 = obj.add(3, 4);
+        System.out.println(r1);
+        
+    }
+}
