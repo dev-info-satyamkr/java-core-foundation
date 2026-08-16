1cfeb4625c0ee0415cb062474714463f12bb2533
dev-info-satyamkr <sy250957@gmail.com>
2026-08-16 19:57:10 +0530

    Final keyword in Java

---
+++ series57.java
@@ -0,0 +1,27 @@
+//  final - variable , method , class
+
+
+final class Calc
+ {
+   public void show()
+   {
+      System.out.println("By Satyam");
+   }    
+      public void add(int a,int b)
+      {
+        System.out.println(a+b);
+      }
+   }
+
+
+   public class series57
+   {
+      public static void main(String a[])
+      {
+         Calc obj = new Calc();
+         obj.show();
+         obj.add(10 , 45);
+      }
+   }
+
+
