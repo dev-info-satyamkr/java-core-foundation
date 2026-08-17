1daba3fd1523bbddf3538f217d6b476de62ae1b1
dev-info-satyamkr <sy250957@gmail.com>
2026-08-17 14:05:32 +0530

    Wrapper Class in Java

---
+++ series60.java
@@ -0,0 +1,18 @@
+public class series60 
+{
+    public static void main(String a[])
+    {
+    
+    int num = 7;
+    Integer num1 = num;    // Autoboxing
+
+    int num2 = num1;      // auto - unboxing
+
+    System.out.println(num2);
+
+    String str = "12";
+    int num3 = Integer.parseInt(str);
+
+    System.out.println(num3*7);
+}
+}
