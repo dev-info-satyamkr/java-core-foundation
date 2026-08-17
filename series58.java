e469bf8645e26bd1e978347d60a19395ea058644
dev-info-satyamkr <sy250957@gmail.com>
2026-08-17 12:34:26 +0530

    Object Class equals toString hashcode in Java

---
+++ series58.java
@@ -0,0 +1,55 @@
+ class Laptop
+  {
+   String model;
+   int price;   
+
+  
+   public String toString() {
+      return "Laptop [model=" + model + ", price=" + price + "]";
+   }
+    
+    
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((model == null) ? 0 : model.hashCode());
+      result = prime * result + price;
+      return result;
+    }
+    
+    public boolean equals(Object obj) {
+      if (this == obj)
+         return true;
+      if (obj == null)
+         return false;
+      if (getClass() != obj.getClass())
+         return false;
+      Laptop other = (Laptop) obj;
+      if (model == null) {
+         if (other.model != null)
+            return false;
+      } else if (!model.equals(other.model))
+         return false;
+      if (price != other.price)
+         return false;
+      return true;
+    }
+}
+
+public class series58
+{
+   public static void main(String a[])
+   {
+      Laptop obj = new Laptop();
+      obj.model = "Asus";
+      obj.price = 90000;
+
+      Laptop obj2 = new Laptop();
+      obj2.model = "Asus";
+      obj2.price = 90000;
+
+      boolean result = obj.equals(obj2);
+
+      System.out.println(result);
+   }
+}
