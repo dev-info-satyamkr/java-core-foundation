692f742998d528c9a857a28b685d41c01b340845
dev-info-satyamkr <sy250957@gmail.com>
2026-08-23 18:55:01 +0530

    Exception Handling Using try catch in Java

---
+++ series77.java
@@ -0,0 +1,20 @@
+public class series77
+ {
+   public static void main(String a[])
+   {
+    int j = 5;
+    int i = 0;
+
+    try 
+    {
+        j = 18/i;
+    }
+    catch(Exception e)
+    {
+        System.out.println("Somethinbg went wrong...");
+    }
+       System.out.println(j); 
+       System.out.println("GoodBye"); 
+
+}
+}
