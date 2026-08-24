f84051e530924559e443b354ac46216b149fe7b8
dev-info-satyamkr <sy250957@gmail.com>
2026-08-24 19:17:44 +0530

    Runnable vs Thread in Java

---
+++ series88.java
@@ -0,0 +1,32 @@
+public class series88
+ {
+   public static void main(String a[])
+    {
+      Runnable obj1 = () ->
+      {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Satyam");
+            try{ Thread.sleep(10);}catch(InterruptedException e) { e.printStackTrace();
+            }
+        }
+      };
+
+       Runnable obj2 = () ->
+      {
+        for(int i=1;i<=5;i++)
+         {
+            System.out.println("Yadav");
+            try{ Thread.sleep(10);}catch(InterruptedException e) { e.printStackTrace();
+            }
+        }
+      };
+
+            Thread t1 = new Thread(obj1);
+            Thread t2 = new Thread(obj2);
+            t1.start();
+            t2.start();
+    }
+}
+       
+
