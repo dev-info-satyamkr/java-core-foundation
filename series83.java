ccf40f1d534a9e1c0137238753003df3a145cb10
dev-info-satyamkr <sy250957@gmail.com>
2026-08-24 14:13:32 +0530

    User Input using BufferedReader and Scanner in Java

---
+++ series83.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class series83
+{
+    public static void main(String a[]) throws IOException
+    {
+        System.out.println("Enter the number");
+
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // BufferedReader bf = new BufferedReader(in);
+
+        Scanner sc = new Scanner(System.in);
+         
+        int num = sc.nextInt();
+        System.out.println(num);
+
+    
+
+    }
+}
+
