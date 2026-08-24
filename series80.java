9488e78d3c62061b11b5cbb2111f7982c4652a32
dev-info-satyamkr <sy250957@gmail.com>
2026-08-24 13:10:23 +0530

    Exception throw keyword in Java

---
+++ series80.java
@@ -0,0 +1,27 @@
+public class series80
+{
+  public static void main(String a[])
+  {
+    int i = 20;
+    int j = 0;
+
+    try
+    {
+        j = 18/i;
+        if(j==0)
+            throw new ArithmeticException("I don't want to print zero");
+    }
+        catch(ArithmeticException e)
+        {
+            j = 18/1;
+            System.out.println("It is default value" + e);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong" + e);
+        }
+
+        System.out.println(j);
+        System.out.println("Bye");
+  }  
+}
