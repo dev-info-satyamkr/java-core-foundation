e9ec10d60a80161dda0965eb570392ef5b3f9327
dev-info-satyamkr <sy250957@gmail.com>
2026-08-25 13:30:13 +0530

    ArrayList in Java

---
+++ series92.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class series92
+ {
+  public static void main(String a[])
+  {
+    List<Integer> nums = new ArrayList<Integer>();
+    nums.add(6);
+    nums.add(9);
+    nums.add(10);
+    nums.add(45);
+    
+    System.out.println(nums.indexOf(9));
+
+    System.out.println(nums.get(2));
+  }  
+}
