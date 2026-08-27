3fd32ef6f2b50cc4f2b34809c56b223d6dd84c9e
dev-info-satyamkr <sy250957@gmail.com>
2026-08-27 18:52:44 +0530

    forEach Method in Java

---
+++ series97.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class series97 {
+    public static void main(String a[])
+    {
+        List<Integer> nums = Arrays.asList(4,5,7,3,2,6);
+        nums.forEach(n -> System.out.println(n));
+    }
+    
+}
